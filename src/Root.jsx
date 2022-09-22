ec6227b13368ff842390e5b2bb6027a619d75f18
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-22 12:00:29 +0600

    build: routes

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#333" }}>
+        NavBar
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetails />} />
+          <Route path="/channel/:id" element={<ChannelDetails />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default Root
+export default Root;
