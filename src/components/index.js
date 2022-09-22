02207229e22190866302c8027dfb063509eb38ae
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-22 12:56:02 +0600

    ci: sidebar component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as ChannelDetails } from "./ChannelDetails";
 export { default as VideoDetails } from "./VideoDetails";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as Searchbar } from "./Searchbar";
+export { default as Sidebar } from "./Sidebar";
