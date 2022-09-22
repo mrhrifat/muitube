ce705ccb1041f47c25d3898df79a0b3a98492135
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-22 12:02:25 +0600

    ci: import all components path

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,17 @@
 import { Box } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetails from "./components/VideoDetails";
+import ChannelDetails from "./components/ChannelDetails";
+import Feed from "./components/Feed";
 
 const Root = () => {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "#333" }}>
-        NavBar
+        <Navbar />
         <Routes>
           <Route path="/" element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetails />} />
