04db0cc5af7a6f7699db85b045275e8628f0c55d
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-22 12:56:15 +0600

    build: feed with sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Sidebar } from "./";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sm: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sm: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sm: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          variant="body2"
+          className="copyright"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 Mrh Rifat
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
