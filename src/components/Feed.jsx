8da9639fcd139e7faa43ea418c454cf4d7f3bf39
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-22 13:29:31 +0600

    build: sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,6 +21,18 @@ const Feed = () => {
           Copyright 2022 Mrh Rifat
         </Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New
+          <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+      </Box>
     </Stack>
   );
 };
