8697cb501cf192a53013ad8a481594aee098900d
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-25 12:15:39 +0600

    build: export of component

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,5 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as Searchbar } from "./Searchbar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
