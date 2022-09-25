bb9fb5a4376fc176cceba4f258bb9cb4abf86ae8
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-25 13:28:04 +0600

    build: data exists

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,13 +1,13 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
-import {VideoCard,ChannelCard} from "./";
+import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
   console.log(videos);
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((video, index) => (
-        <Box key={{ index }}>
+      {videos?.map((video, index) => (
+        <Box key={index}>
           {video.id.videoId && <VideoCard video={video} />}
           {video.id.channelId && <ChannelCard channel={video} />}
         </Box>
