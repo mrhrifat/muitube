40aee9086749b1e88ddda164cd06125af53a258d
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-25 12:30:08 +0600

    build: video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { CheckCircle } from "@mui/icons-material";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CartMedia,
+  CardMedia,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
 
-const VideoCard = () => {
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  //   console.log(videoId, snippet);
+
   return (
-    <div>VideoCard</div>
-  )
-}
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+    </Card>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
