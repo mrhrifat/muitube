d3a7cb9fa0bf331d8f836ed1a35d18d976914ac6
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-25 12:13:59 +0600

    build: video & channel render box

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { Box, Stack } from "@mui/material";
+import React from "react";
 
-const Videos = ({videos}) => {
-  console.log(videos)
+const Videos = ({ videos }) => {
+  console.log(videos);
   return (
-    <div>Videos</div>
-  )
-}
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((video, index) => (
+        <Box key={{ index }}>
+          {video.id.videoId && <VideoCard video={video} />}
+          {video.id.channelId && <ChannelCard channel={video} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default Videos
+export default Videos;
