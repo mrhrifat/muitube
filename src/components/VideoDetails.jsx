6f930b2c19e536a5a8d58972ff08c9b1f58bb7ee
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-27 16:44:11 +0600

    remove: loading component

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import { Videos, Loader } from "./";
+import { Videos } from "./";
 import { fetchData } from "../data/fetchData";
 
 const VideoDetail = () => {
