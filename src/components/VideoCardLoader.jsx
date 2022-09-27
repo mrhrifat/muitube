d230e8d286c83f7c1ae719aa43a605ac21ee6f08
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-27 16:44:47 +0600

    build: video card loader

--- src/components/VideoCardLoader.jsx
+++ src/components/VideoCardLoader.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Card, Skeleton, Stack } from "@mui/material";
+import React from "react";
+
+const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const VideoCardLoader = () => {
   return (
-    <div>VideoCardLoader</div>
-  )
-}
+    <Stack direction="row" gap={2} flexWrap="wrap">
+      {videos.map((video) => (
+        <Box sx={{ padding: 1 }} key={video}>
+          <Skeleton width={320} height={280} />
+          <Skeleton variant="text" />
+          <Skeleton variant="text" width="50%" />
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default VideoCardLoader
+export default VideoCardLoader;
